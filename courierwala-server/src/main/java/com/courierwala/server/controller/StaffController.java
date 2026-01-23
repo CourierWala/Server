6334b3a64de526e927755a8d930d6873bfb6f0dd
Harsh_rode <rodeharsh.0027@gmail.com>
2026-01-24 01:14:10 +0530

    implemented login-staff, signup-staff and staff profile APIs

--- courierwala-server/src/main/java/com/courierwala/server/controller/StaffController.java
+++ courierwala-server/src/main/java/com/courierwala/server/controller/StaffController.java
@@ -1,14 +1,36 @@
 package com.courierwala.server.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.courierwala.server.customerdto.LoginDTO;
+import com.courierwala.server.customerdto.SignUpDTO;
+import com.courierwala.server.dto.ApiResponse;
+import com.courierwala.server.entities.DeliveryStaffProfile;
+import com.courierwala.server.entities.User;
+import com.courierwala.server.service.CustomerService;
+import com.courierwala.server.service.StaffService;
+import com.courierwala.server.staffdto.StaffSignupDto;
+import com.courierwala.server.staffdto.staffProfileResponseDTO;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/staff")
+@RequiredArgsConstructor
 public class StaffController {
+	
+	@Autowired
+	public final StaffService staffservice;
 
 	@GetMapping("/{staffid}")
 	public String getAllOrdersByStaff() {
@@ -34,21 +56,70 @@ public class StaffController {
 		return "confirmation msg: Order Delivered";
 	}
 	
-	@GetMapping("/profile/{staffid}")
-	public String getStaffProfile() {
-		//TODO : create a responce = get staff profile 
-		return "staff details";
+	@GetMapping("/profile/{staffId}")
+	public ResponseEntity<?> getStaffProfile(@PathVariable Long staffId) {
+	    try {
+	        staffProfileResponseDTO response =
+	                staffservice.getStaffProfile(staffId);
+
+	        return ResponseEntity.status(HttpStatus.OK).body(response);
+	               
+	    } catch (RuntimeException e) {
+	        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+	                .body(new ApiResponse(e.getMessage(), "Failed"));
+	    }
 	}
+
 	
-	@PostMapping("/profile/{staffid}")
-	public String updateStaffProfile() {
-		//TODO : update staff profile
-		return "cnf msg";
-	}
+//	@PostMapping("/profile/{staffId}")
+//	public String updateStaffProfile(@PathVariable Long staffId) {
+//		try {
+//	        DeliveryStaffProfile profile = staffservice.getStaffProfile(staffId);
+//	        return ResponseEntity.ok(
+//	                new ApiResponse("staff profile fetched successfully", profile)
+//	        );
+//	    } catch (RuntimeException e) {
+//	        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+//	                .body(new ApiResponse(e.getMessage(), "Failed"));
+//	    }
+//	}
 	
 	@GetMapping("/earnings/{staffid}")
 	public String getCompletedOrdersByStaff() {
 		//TODO : create a responce = list of all orders with status in_transit and staffid = staffid 
 		return "orderList";
 	}
+	
+
+	
+	
+	@PostMapping("/signup")
+    public ResponseEntity<?> signUp(
+            @Valid @RequestBody StaffSignupDto signupdto){
+        try {
+        	   staffservice.signUp(signupdto);
+			return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse("signup sucessfully", "success"));
+		} catch (RuntimeException e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND)
+					.body(new ApiResponse(e.getMessage(), "Failed"));
+		}
+    }
+	
+	@PostMapping("/login")
+    public ResponseEntity<?> login(
+            @Valid @RequestBody LoginDTO loginDTO) {
+		
+		try {
+			staffservice.login(loginDTO);
+			return ResponseEntity.status(HttpStatus.ACCEPTED).body(new ApiResponse("login sucessfully", "success"));
+		} catch (RuntimeException e) {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+					.body(new ApiResponse(e.getMessage(), "Failed"));
+		}
+
+         
+
+        
+    }
+	
 }
