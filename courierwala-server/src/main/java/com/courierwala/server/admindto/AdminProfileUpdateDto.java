b82b0e820e30197ae4ab44b4b77c723bae2a7752
omkharmate <omkharmate85@gmail.com>
2026-01-27 16:17:57 +0530

    resolved some conflicts happened due to changed UserRepo name, unneccesaory imports etc

---
+++ courierwala-server/src/main/java/com/courierwala/server/admindto/AdminProfileUpdateDto.java
@@ -0,0 +1,11 @@
+package com.courierwala.server.admindto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AdminProfileUpdateDto {
+    private String name;
+    private String email;
+}
