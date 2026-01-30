44c488a6d1c6c58c12d063d6d68b31f905fe5a99
sujay <sujaygangan2@gmail.com>
2026-01-30 11:12:08 +0530

    Almost completed new job application acception and rejection

---
+++ courierwala-server/src/main/java/com/courierwala/server/controller/ManagerController.java
@@ -0,0 +1,46 @@
+package com.courierwala.server.controller;
+
+import com.courierwala.server.dto.ApiResponse;
+import com.courierwala.server.dto.GetStaffDto;
+import com.courierwala.server.service.ManagerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(
+        origins = "http://localhost:5173",
+        allowCredentials = "true"
+)
+@RestController
+@RequestMapping("/api/manager")
+@RequiredArgsConstructor
+public class ManagerController {
+
+    private final ManagerService managerService;
+
+    @GetMapping("/acceptStaff/{applicationId}")
+    public ResponseEntity<ApiResponse> acceptDeliveryStaff(
+            @PathVariable Long applicationId) {
+
+        managerService.acceptDeliveryStaff(applicationId);
+
+        return ResponseEntity.ok(
+                new ApiResponse(
+                        "Manager details updated successfully",
+                        "SUCCESS"
+                )
+        );
+    }
+
+    @GetMapping("/applications")
+    public List<GetStaffDto> getAllJobApplications() {
+        return managerService.getAllStaff(false);
+    }
+
+    @GetMapping("/current-staff")
+    public List<GetStaffDto> getAllCurrentStaff() {
+        return managerService.getAllStaff(true);
+    }
+}
