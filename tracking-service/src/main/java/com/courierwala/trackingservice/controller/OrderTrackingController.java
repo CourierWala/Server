5a877853f6cda983a5b07ad2bf7b12d6c844a77c
Omkar Mane <omkarmane8603@gmail.com>
2026-01-31 01:14:15 +0530

    Status update event publish from Staff Service

--- tracking-service/src/main/java/com/courierwala/trackingservice/controller/OrderTrackingController.java
+++ tracking-service/src/main/java/com/courierwala/trackingservice/controller/OrderTrackingController.java
@@ -5,16 +5,21 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.courierwala.trackingservice.dto.TrackingResponse;
-import com.courierwala.trackingservice.entities.OrderStatusHistory;
 import com.courierwala.trackingservice.service.OrderTrackingService;
 
 import lombok.RequiredArgsConstructor;
+
+@CrossOrigin(
+	    origins = "http://localhost:5173/",
+	    allowCredentials = "true"
+	)
 
 @RestController
 @RequiredArgsConstructor
@@ -29,7 +34,7 @@ public class OrderTrackingController {
  		TrackingResponse trackingResponce = orderTrackingService.getTrackingByOrderId(orderId);
  		return ResponseEntity.ok(trackingResponce);
 	} catch (Exception e) {
-		return ResponseEntity.status(HttpStatus.NO_CONTENT).body("No Order Found with this Tracking Number !!");
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No Order Found with this Tracking Number !!");
 	}
 		
 	}
