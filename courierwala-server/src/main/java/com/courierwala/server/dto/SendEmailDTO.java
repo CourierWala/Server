a52c1dcae02991190a4740b4b2f89e5c8275cb11
Prashantkumar-Dudhmal <prashantkumardudhmal@gmail.com>
2026-01-31 13:38:03 +0530

    EmailService Added

---
+++ courierwala-server/src/main/java/com/courierwala/server/dto/SendEmailDTO.java
@@ -0,0 +1,19 @@
+package com.courierwala.server.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class SendEmailDTO {
+	private String to;
+	private String subject;
+	private String message;
+	public SendEmailDTO(String subject, String message) {
+		this.subject = subject;
+		this.message = message;
+	}
+	
+}
